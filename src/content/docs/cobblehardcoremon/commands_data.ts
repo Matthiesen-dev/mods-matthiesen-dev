095659384b9e6b47a914ee3e35a3b0d5b94d3b14
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-15 00:40:21 -0700

    Add getHealthLink and getHealthLinkOther commands with permissions to CobbleHardcoreMon

--- src/content/docs/cobblehardcoremon/commands_data.ts
+++ src/content/docs/cobblehardcoremon/commands_data.ts
@@ -14,9 +14,21 @@ export const commands: Commands = [
         permissionNode: "cobblehardcoremon.command.hardcoremon.set-health-link",
     },
     {
+        command: "hardcoremon getHealthLink",
+        description: "Checks whether the Health Link feature is enabled or disabled for the player.",
+        usage: "/hardcoremon getHealthLink",
+        permissionNode: "cobblehardcoremon.command.hardcoremon.get-health-link",
+    },
+    {
         command: "hardcoremon forceHealthLink",
         description: "Forces the Health Link feature to be enabled or disabled for a specific player.",
         usage: "/hardcoremon forceHealthLink <player> <true|false>",
         permissionNode: "cobblehardcoremon.command.hardcoremon.force-health-link",
+    },
+    {
+        command: "hardcoremon getHealthLinkOther",
+        description: "Checks whether the Health Link feature is enabled or disabled for a specific player.",
+        usage: "/hardcoremon getHealthLinkOther <player>",
+        permissionNode: "cobblehardcoremon.command.hardcoremon.get-health-link-other",
     }
 ];
